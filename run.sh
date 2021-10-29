c6f5d3ed1f35017b1e84ca6374fd021f1309d34e
topcue <topcue1019@naver.com>
2021-10-29 21:56:01 +0900

    Update file run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$HOME/Pin/pin-3.20/pin -t ./profiler/obj-intel64/profiler.so -c -s -- /bin/apt
+$HOME/Pin/pin-3.20/pin -t ./profiler/obj-intel64/profiler.so -c -s -- ./test_bin/apt
 
 # EOF
 
